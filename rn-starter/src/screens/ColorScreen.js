155a249ffd0b8b05ec31d27b7a0f3767b138aa5d
Ashley <thy.ashley@gmail.com>
2020-09-23 20:25:42 +0800

    added random color generator

--- rn-starter/src/screens/ColorScreen.js
+++ rn-starter/src/screens/ColorScreen.js
@@ -1,10 +1,32 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, StyleSheet, Button, FlatList } from "react-native";
 const ColorScreen = () => {
-  const [color, setColor] = useState([]);
+  const [colors, setColors] = useState([]);
+
+  const randomColor = () => {
+    const red = Math.floor(Math.random() * 255);
+    const green = Math.floor(Math.random() * 255);
+    const blue = Math.floor(Math.random() * 255);
+    setColors([...colors, `rgb(${red}, ${green}, ${blue})`]);
+  };
   return (
     <View>
-      <Button title="Add a Color" />
+      <Button title="Add a Color" onPress={randomColor} />
+      <FlatList
+        keyExtractor={(item) => item}
+        data={colors}
+        renderItem={({ item }) => {
+          return (
+            <View
+              style={{
+                width: 100,
+                height: 100,
+                backgroundColor: item,
+              }}
+            />
+          );
+        }}
+      />
     </View>
   );
 };
