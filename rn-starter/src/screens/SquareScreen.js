db9f558b3ab68d623da2eaa2857f334a255dbd53
Ashley <thy.ashley@gmail.com>
2020-09-23 23:23:28 +0800

    refactor code

--- rn-starter/src/screens/SquareScreen.js
+++ rn-starter/src/screens/SquareScreen.js
@@ -2,24 +2,30 @@ import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import ColorAdjuster from "../components/ColorAdjuster";
 
+const colorIncrement = 12;
 const SquareScreen = () => {
   const [red, setRed] = useState(0);
   const [green, setGreen] = useState(0);
   const [blue, setBlue] = useState(0);
   return (
     <View>
-      <ColorAdjuster userColor="Red" color={red} setColor={setRed} value={12} />
+      <ColorAdjuster
+        userColor="Red"
+        color={red}
+        setColor={setRed}
+        value={colorIncrement}
+      />
       <ColorAdjuster
         userColor="Green"
         color={green}
         setColor={setGreen}
-        value={12}
+        value={colorIncrement}
       />
       <ColorAdjuster
         userColor="Blue"
         color={blue}
         setColor={setBlue}
-        value={12}
+        value={colorIncrement}
       />
       <View
         style={{
