8bb330b9f5657be5b7437e22abe41c1d64b2c33f
Ashley <thy.ashley@gmail.com>
2020-09-30 12:51:17 +0800

    added marketplace app

---
+++ marketplace/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {
+  Platform,
+  SafeAreaView,
+  StyleSheet,
+  View,
+  StatusBar,
+} from "react-native";
+
+export default function App() {
+  return null;
+}
