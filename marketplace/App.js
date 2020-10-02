2f0ffd8815355685f8bd5cf2fbef7d63a4c1ba1d
Ashley <thy.ashley@gmail.com>
2020-10-02 12:10:12 +0800

    added fixes to touch up message component

--- marketplace/App.js
+++ marketplace/App.js
@@ -2,7 +2,8 @@ import React from "react";
 import ListingEditScreen from "./src/screens/ListingEditScreen";
 
 import LoginScreen from "./src/screens/LoginScreen";
+import MessagesScreen from "./src/screens/MessagesScreen";
 
 export default function App() {
-  return <ListingEditScreen />;
+  return <MessagesScreen />;
 }
--- marketplace/src/components/ListItem/index.js
+++ marketplace/src/components/ListItem/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Image, StyleSheet, TouchableHighlight, View } from "react-native";
 import Swipeable from "react-native-gesture-handler/Swipeable";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import colors from "../../config/colors";
 import AppText from "../AppText";
@@ -20,9 +21,20 @@ export default function ListItem({
           {IconComponent}
           {image && <Image style={styles.image} source={image} />}
           <View style={styles.detailsContainer}>
-            <AppText style={styles.title}>{title}</AppText>
-            {subTitle && <AppText style={styles.subtitle}>{subTitle}</AppText>}
+            <AppText numberOfLines={1} style={styles.title}>
+              {title}
+            </AppText>
+            {subTitle && (
+              <AppText numberOfLines={2} style={styles.subtitle}>
+                {subTitle}
+              </AppText>
+            )}
           </View>
+          <MaterialCommunityIcons
+            color={colors.medium}
+            name="chevron-right"
+            size={25}
+          />
         </View>
       </TouchableHighlight>
     </Swipeable>
@@ -34,10 +46,12 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     padding: 15,
     backgroundColor: colors.white,
+    alignItems: "center",
   },
   detailsContainer: {
     marginLeft: 10,
     justifyContent: "center",
+    flex: 1,
   },
   image: {
     borderRadius: 35,
