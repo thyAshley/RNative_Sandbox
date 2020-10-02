b6bdf8de35359ce6acfd3bff23424da47630352b
Ashley <thy.ashley@gmail.com>
2020-10-02 21:39:43 +0800

    added stack navigations

--- marketplace/App.js
+++ marketplace/App.js
@@ -1,10 +1,50 @@
-import React, { useEffect, useState } from "react";
-
+import React from "react";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import Screen from "./src/components/SafeScreen";
-import ImageInput from "./src/components/ImageInput";
-import ImageInputList from "./src/components/ImageInputList/Index";
-import ListingEditScreen from "./src/screens/ListingEditScreen";
+import { Button, Text } from "react-native";
+
+const Link = () => {
+  const navigation = useNavigation();
+  return (
+    <Button title="Click" onPress={() => navigation.navigate("TweetDetail")} />
+  );
+};
+
+const Tweets = ({ navigation }) => {
+  return (
+    <Screen>
+      <Text>Tweets</Text>
+      <Button
+        title="Details"
+        onPress={() => navigation.navigate("TweetDetail")}
+      />
+    </Screen>
+  );
+};
+
+const TweetDetails = () => {
+  return (
+    <Screen>
+      <Text>Tweet Details</Text>
+    </Screen>
+  );
+};
+
+const AppStack = createStackNavigator();
+const StackNavigator = () => {
+  return (
+    <AppStack.Navigator>
+      <AppStack.Screen name="Tweets" component={Tweets} />
+      <AppStack.Screen name="TweetDetail" component={TweetDetails} />
+    </AppStack.Navigator>
+  );
+};
 
 export default function App() {
-  return <ListingEditScreen />;
+  return (
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
+  );
 }
